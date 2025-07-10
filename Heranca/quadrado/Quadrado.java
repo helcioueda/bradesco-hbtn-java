8140b64ba953e6175416576b124e64384fd10955
Helcio Fernando Ueda <helcio.ueda@bradesco.com.br>
2025-07-09 21:29:36 -0300

    quadrado

---
+++ Heranca/quadrado/Quadrado.java
@@ -0,0 +1,26 @@
+public class Quadrado extends Retangulo {
+    private double lado;
+
+    public Quadrado() {
+        super(); 
+        this.lado = 0.0;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if (lado < 0) {
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        }
+        this.lado = lado;
+        this.largura = lado;
+        this.altura = lado;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Quadrado] %.2f", lado);
+    }
+}
