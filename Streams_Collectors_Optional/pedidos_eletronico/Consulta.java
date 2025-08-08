285fb49b04cd93923bc78e7a5f48f2aeb50b5b0d
Helcio Fernando Ueda <helcio.ueda@bradesco.com.br>
2025-08-08 19:35:05 -0300

    Streams_Collectors_Optional_pedidos_eletronico

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -0,0 +1,34 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+	
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+    
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        Optional<Produto> produtoMaiorPreco = produtos.stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst();
+
+        return produtoMaiorPreco.orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        return produtos.stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos.stream()
+                .filter(pedido -> pedido.getProdutos().stream()
+                                         .anyMatch(p -> p.getCategoria() == CategoriaProduto.ELETRONICO))
+                .collect(Collectors.toList());
+    }
+}
