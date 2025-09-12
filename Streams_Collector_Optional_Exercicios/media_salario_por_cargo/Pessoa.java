274d7864696fb8eefa38d9ffb5528b28f05327cf
Helcio Fernando Ueda <helcio.ueda@bradesco.com.br>
2025-09-12 19:32:45 -0300

    media_salario_por_cargo

---
+++ Streams_Collector_Optional_Exercicios/media_salario_por_cargo/Pessoa.java
@@ -0,0 +1,58 @@
+import java.util.Locale;
+import java.util.Objects;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+    
+    
+
+    public double getSalario() {
+		return salario;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	@Override
+    public String toString() {
+    	Locale.setDefault(new Locale("pt", "BR"));
+    	
+        return String.format(Locale.getDefault(), "[%d] %s %s %d R$ %.6f",
+            this.codigo, this.nome, this.cargo, this.idade, this.salario);
+    }
+
+    @Override
+    public int compareTo(Pessoa outra) {
+        return this.nome.compareTo(outra.nome);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa)) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return codigo == pessoa.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
